90b50612e1df07383d5c5b30777a398066520a20
Tech Global <techglobal.school@gmail.com>
2022-03-01 20:43:43 -0600

    added the test with the ExcelUtils and DataProviderUtils

--- src/test/java/utils/DataProviderUtils.java
+++ src/test/java/utils/DataProviderUtils.java
@@ -11,6 +11,9 @@ public class DataProviderUtils {
         // Opening the exel file
         ExcelUtils.openExcelFile("petStoreData", "Sheet1");
         // Converting the list of list into multidimensional Object array
+            // Following code will run the test with the method name in the test and Excel sheet
+//        Object[][] arrayObject = ExcelUtils.getExcelData(ExcelUtils.getValues(method.getName()));
+        // Following code will run the test with regardless of the method name in the test class
         Object[][] arrayObject = ExcelUtils.getExcelData(ExcelUtils.getValues());
         // Closing the Excel file
         ExcelUtils.closeExcelFile();
